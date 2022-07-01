9d800300b098b6db4168358dec571d169f9aa12e
Brian Kariuki <107594995+DeveloperAstra@users.noreply.github.com>
2022-07-02 01:21:28 +0300

    Add files via upload

--- vendor/includes/db.php
+++ vendor/includes/db.php
@@ -1,8 +1,6 @@
 <?php 
 
-
-$db = mysqli_connect('sql8.freemysqlhosting.net', 'sql8503027', 'kJrkGAFNZ2', 'onekenya') or die("Couldn't connect to the database");
-
+$db = mysqli_connect('sql8.freemysqlhosting.net', 'sql8503027', 'kJrkGAFNZ2', 'sql8503027') or die("Couldn't connect to the database");
 
 
 ?>
